ff3b63760fff31de966b9999887fd0bf3dfe6795
pc-dok <info@n4k.at>
2020-04-25 19:08:07 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,19 +3,17 @@
 #terraform {
 #  backend "remote" {
 #    hostname     = "app.terraform.io"
-#    organization = "N4K"
+#    organization = "yourID"
 #
 #    workspaces {
-#      name = "N4K-UAC-AF1"
+#      name = "yourRepo"
 #    }
 #  }
 #}
 
-//Note: This is only my Azure Login Credentials for my UMB Visual Studio Enterprise MPN Account
-
 provider "azurerm" {
   #version = "=2.7"
-  subscription_id = "18074942-d5bd-48bc-af90-4af06d9526d5"
-  tenant_id       = "8e7d5e9c-e72c-4d0b-9a7e-34aba5e8581c"
+  subscription_id = "yourId"
+  tenant_id       = "yourID"
   features {}
 }
